d1960f7e53c66a0a062cd2c36826216d03d9645c
ankita-2608 <118102456+ankita-2608@users.noreply.github.com>
2023-01-08 17:57:29 +0530

    Create ListSTL.cpp

---
+++ ListSTL.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<list>
+
+using namespace std;
+
+int main() {
+    list<int> l;
+
+    list<int> n(5, 100);
+    for(int i:n) {
+        cout<<i<<" ";
+    }cout<<endl;
+
+    list<int> d(l);
+
+    l.push_back(1);
+    l.push_front(2);
+
+    for(int i:l) {
+        cout<<i<<" ";
+    }cout<<endl;
+
+    l.erase(l.begin());
+
+    for(int i:l) {
+        cout<<i<<" ";
+    }cout<<endl;
+
+    cout<<"Size of list "<<l.size()<<endl;
+
+    return 0;
+}
