0ed3d50aad7dc6abb54ca2fe8b118ddc40977779
ankita-2608 <118102456+ankita-2608@users.noreply.github.com>
2023-01-08 18:03:16 +0530

    Create MapSTL.cpp

---
+++ MapSTL.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+int main() {
+    map<int, string> m;
+    m[1] = "Ankita";
+    m[2] = "Singh";
+    m[13] = "Chandel";
+
+    m.insert({5, "bheem"});
+
+    for(auto i:m) {
+        cout<<i.first<<" "<<i.second<<endl;
+    }cout<<endl;
+
+    cout<<"Finding 13 "<<m.count(13)<<endl;
+
+    cout<<"Before erase"<<endl;
+    for(auto i:m) {
+        cout<<i.first<<" "<<i.second<<endl;
+    }cout<<endl;
+    m.erase(13);
+    cout<<"After  erase"<<endl;
+    for(auto i:m) {
+        cout<<i.first<<" "<<i.second<<endl;
+    }cout<<endl;
+
+    auto it = m.find(2);
+
+    for(auto i=it; i!=m.end(); i++) {
+        cout<<(*i).first<<endl;
+    }
+
+    return 0;
+}
